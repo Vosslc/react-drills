19c5cebd6d33c0615c16a35c9f3b7bf7aa91268e
Nate Roundy <nroundy801@gmail.com>
2019-11-10 12:00:13 -0700

    DONE

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -1,18 +1,42 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import axios from 'axios';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        starWars: '',
+        planet: ''
+      }
+  }
+  
+  componentDidMount(){
+    axios.get('https://swapi.co/api/people/1')
+    .then(res => {
+      this.setState({
+        starWars: res.data
+      });
+    }),
+    axios.get("https://swapi.co/api/planets/1/")
+    .then(res => {
+      this.setState({
+        planet: res.data
+      });
+    })
+  }
+
+
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Name: {this.state.starWars.name}</h1>
+        <h2>Hair_color: {this.state.starWars.hair_color}</h2>
+        <h2>birth_year: {this.state.starWars.birth_year}</h2>
+        <h2>planet: {this.state.planet.name}</h2>
+        
       </div>
     );
   }
